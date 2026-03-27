e3df513937786b7de455b16aa671f031b9a40011
David Bryan <davidbryanj.76@gmail.com>
2026-03-26 22:28:50 -0300

    Aula de JS

---
+++ aula02/funcoes.js
@@ -0,0 +1,26 @@
+// Pascal é um padrão de escrita que separa as
+// palavras onde toda senteça inicia em maiusculo.
+
+function SomaNumeros () {
+    let valor_i = 6
+    let valor_ii = 8
+    let resultado = 0
+
+    return resultado = valor_i + valor_ii
+}
+
+console.log(SomaNumeros())
+
+function SomaNumerosII (valor_i, valor_ii) {
+    console.log(valor_i + valor_ii)
+}
+
+SomaNumerosII(9, 15)
+SomaNumerosII(14, 10)
+SomaNumerosII(45, 40)
+
+const SOMA_NUMEROS_III = (valor_i, valor_ii) => {
+    return valor_i + valor_ii
+}
+
+console.log(SOMA_NUMEROS_III(81 + 98))
---
+++ aula02/numeros.js
@@ -0,0 +1,36 @@
+// Snakeb Case é caracterizado por utilizar o underline/underscore (_)
+// para separar suas palavras. Pode ser combinado entre letras 
+// maisculuas (SCREAMING) ou miniculas.
+let primeiro_Valor = 0
+let segundo_Valor = 0
+
+primeiro_Valor = 4
+segundo_Valor = 5
+
+
+    console.log(
+        '${primeiro_Valor} + ${segundo_Valor} = ',
+        primeiro_Valor + segundo_Valor
+
+    )
+
+console.log(
+    '${primeiro_Valor} - ${segundo_Valor} = ',
+    primeiro_Valor - segundo_Valor
+
+)
+
+console.log(
+    '${primeiro_Valor} * ${segundo_Valor} = ',
+    primeiro_Valor * segundo_Valor
+
+)
+
+console.log(
+    '${primeiro_Valor} / ${segundo_Valor} = ',
+    primeiro_Valor / segundo_Valor
+
+)
+
+let variavel = "Oi"
+console.log(typeof(variavel))
